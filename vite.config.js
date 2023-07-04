a5787bf8886992746da697857dc2e15a3e178c64
fourside <fourside@gmail.com>
2023-07-04 10:07:36 +0900

    vite.config.js generate manifest.json

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { crx, defineManifest } from "@crxjs/vite-plugin";
+import { defineConfig } from "vite";
+import packageJson from "./package.json";
+
+const manifest = defineManifest({
+  manifest_version: 3,
+  name: "Monthly debt hours",
+  version: packageJson.version,
+  icons: {
+    16: "images/icon-16.svg",
+    32: "images/icon-32.svg",
+  },
+  permissions: ["activeTab"],
+  action: {
+    default_title: "Monthly debt hours",
+    default_popup: "index.html",
+  },
+});
+
+export default defineConfig({
+  plugins: [crx({ manifest })],
+});
